9e1fa86883e53df8c1e0b1832454ca04910dbc1a
05Winel05 <150167710+05Winel05@users.noreply.github.com>
2023-11-11 12:55:34 +0600

    canteen

---
+++ Canteen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, n;
+    cin >> a >> b >> n;
+    b=(a*100+b)*n;
+    a=b/100;
+    b=b%100;
+    cout << a <<" som and " << b << " tyiyn";
+
+}
